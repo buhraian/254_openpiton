3d44afade1b4ebb02f37b71cdc95f0d0d401c8a1
Brian Li <brianli@ucsb.edu>
2022-03-02 20:27:39 -0800

    middle of week 9

--- piton/verif/diag/c/riscv/ariane/hello_world_many.c
+++ piton/verif/diag/c/riscv/ariane/hello_world_many.c
@@ -19,18 +19,27 @@
 #include "util.h"
 
 int main(int argc, char** argv) {
-
   // synchronization variable
   volatile static uint32_t amo_cnt = 0;
-
   // synchronize with other cores and wait until it is this core's turn
   while(argv[0][0] != amo_cnt);
-
+  int assembly_output = 5;
+  /* assembly aiming to verify that writing and reading to a USER CSR works as
+  expected */
+  printf("Output coming from inline assembly = %d\n", assembly_output);
+  asm("and x5, x5, 0\n\t" // set the value of x5 to zero
+  "addi x5, x5, 0\n\t" // set the value of x5 to two
+  "csrw 0x808, x5\n\t" // write the value of x5 to the USER CSR 0x800
+  "csrr x5, 0x808\n\t" // read the value that the USER CSR has in order to validate that csrw worked as expected
+  "move %0, x5\n\t"
+  :"=r" (assembly_output) // output
+  );
+  /* print out the value of the assembly_output variable in order to validate
+  that the inline_assembly code worked as expected and modified the contents of the USER CSR */
+  printf("Output coming from inline assembly = %d\n", assembly_output);
   // assemble number and print
   printf("Hello world, this is hart %d of %d harts!\n", argv[0][0], argv[0][1]);
-
   // increment atomic counter
   ATOMIC_OP(amo_cnt, 1, add, w);
-
-  return 0;
+return 0;
 }
